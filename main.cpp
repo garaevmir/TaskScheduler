d43a117839f97ef570c0e511f0f955b1245d4591
garaevmir <aigaraev@edu.hse.ru>
2024-11-03 16:53:08 +0300

    task done

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+class TaskScheduler {
+public:
+    TaskScheduler() {
+        scheduler = std::thread([&] { run(); });
+    }
+
+    ~TaskScheduler() {
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            stop = true;
+        }
+
+        cond.notify_one();
+        if (scheduler.joinable()) {
+            scheduler.join();
+        }
+
+        for (auto& worker : workers) {
+            if (worker.joinable()) {
+                worker.join();
+            }
+        }
+    }
+
+    void Add(std::function<void()> task, std::time_t timestamp) {
+        std::unique_lock<std::mutex> lock(mutex);
+        tasks.emplace(std::move(task), timestamp);
+        cond.notify_one();
+    }
+
+private:
+    struct Task {
+        Task(std::function<void()> _func, std::time_t _time) : func(std::move(_func)), time(_time){};
+
+        std::function<void()> func;
+        std::time_t time;
+
+        bool operator<(const Task& other) const {
+            return other.time < time;
+        }
+    };
+
+    bool stop = false;
+    std::thread scheduler;
+    std::mutex mutex;
+    std::condition_variable cond;
+    std::priority_queue<Task> tasks;
+    std::vector<std::thread> workers;
+
+    void run() {
+        while (true) {
+            std::unique_lock<std::mutex> lock(mutex);
+
+            if (stop && tasks.empty()) {
+                break;
+            } else if (tasks.empty()) {
+                cond.wait(lock);
+            } else {
+                auto curr_time = std::time(nullptr);
+                auto& nearest_task = tasks.top();
+
+                if (nearest_task.time <= curr_time) {
+                    workers.emplace_back(nearest_task.func);
+                    tasks.pop();
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    TaskScheduler scheduler;
+
+    scheduler.Add([] { std::cout << "Task 1\n"; }, std::time(nullptr) + 2);
+    scheduler.Add([] { std::cout << "Task 2\n"; }, std::time(nullptr) + 1);
+    scheduler.Add([] { std::cout << "Task 3\n"; }, std::time(nullptr) + 5);
+    scheduler.Add([] { std::cout << "Task 4\n"; }, std::time(nullptr) + 3);
+
+    return 0;
+}
